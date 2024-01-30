894ef8813561a5a1a92eaf5da0da43a2ef8195da
saum saidah nahiya <saumsaidahnahiua@gmail.com>
2024-01-30 10:17:19 +0300

    structured programming

---
+++ xhaum.py
@@ -0,0 +1,7 @@
+import datetime
+file=open("output.txt","wt")
+file.write("This is python programming")
+file.write(f"\t\tISLAMIC UNIVERSITY IN UGANDA \n\t\t\tKAMPALA CAMPUS \n\t\tNAME:\t\tSAUM SAIDAH NAHIYA \n\t\tREG NO.:\t223-063012-25082 \n\t\tLecturer:\tMr Walusimbi Hakim \n\t\tDate:\t\t{datetime.date.today()} \n\t\tCourse Unit:\tStructured Programming \n\t\t****\t\tEND\t****")
+
+
+
